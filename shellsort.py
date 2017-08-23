af872141ff1d1372c701d8e9c011fe581f9f36ef
root <root@localhost.localdomain>
2017-08-23 17:11:59 -0400

    shellsort

--- shellsort.py
+++ shellsort.py
@@ -10,30 +10,18 @@ class bcolors:
 
 def shellSort(tab):
     #Autorem tej funkcji jest Mohit Kumra, kod podchodzi z geeksforgeeks.org
-    # Start with a big gap, then reduce the gap
     n = len(tab)
     gap = n/2
 
-    # Do a gapped insertion sort for this gap size.
-    # The first gap elements a[0..gap-1] are already in gapped
-    # order keep adding one more element until the entire array
-    # is gap sorted
     while gap > 0:
 
         for i in range(gap,n):
 
-            # add a[i] to the elements that have been gap sorted
-            # save a[i] in temp and make a hole at position i
             temp = tab[i]
-
-            # shift earlier gap-sorted elements up until the correct
-            # location for a[i] is found
             j = i
             while  j >= gap and tab[j-gap] >temp:
                 tab[j] = tab[j-gap]
                 j -= gap
-
-            # put temp (the original a[i]) in its correct location
             tab[j] = temp
         gap /= 2
 
